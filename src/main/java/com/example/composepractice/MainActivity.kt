bd122c39a3143c3b3122bc4c26bc7486c1868339
Saul <82818499+SaulExpo@users.noreply.github.com>
2025-10-17 15:14:52 +0100

    Add files via upload

---
+++ src/main/java/com/example/composepractice/MainActivity.kt
@@ -0,0 +1,246 @@
+package com.example.composepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composepractice.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePracticeTheme {
+                // Pantalla principal
+                AppContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun AppContent() {
+    // Estado para controlar qué pantalla se muestra
+    var screen by remember { mutableStateOf(3) }
+
+    when (screen) {
+        1 -> Screen1 { screen = 2 }
+        2 -> Screen2 { screen = 3 }
+        3 -> Screen3 { screen = 1 }
+    }
+}
+
+@Composable
+fun Screen1(onNext: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val image = painterResource(R.drawable.bg_compose_background)
+        Column (){
+            Image(
+                painter = image,
+                contentDescription = null,
+            )
+            Text(
+                text = "Jetpack Compose tutorial",
+                fontSize = 26.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                )
+            Text(
+                text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                textAlign = TextAlign.Justify,
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp)
+            )
+            Text(
+                text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+                textAlign = TextAlign.Justify,
+                modifier = Modifier
+                    .padding(16.dp)
+            )
+        }
+        Button(onClick = onNext) {
+            Text("Ir a Pantalla 2")
+        }
+    }
+}
+
+@Composable
+fun Screen2(onNext: () -> Unit) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp,
+        )
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Pantalla 2")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onNext) {
+            Text("Ir a Pantalla 3")
+        }
+    }
+}
+
+@Composable
+fun Screen3(onNext: () -> Unit) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 100.dp)
+    ) {
+        Row(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFEADDFF))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Text composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Displays text and follows the recommended Material Design guidelines.",
+                    textAlign = TextAlign.Justify
+
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFD0BCFF))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Image composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Creates a composable that lays out and draws a given Painter class object.",
+                    textAlign = TextAlign.Justify
+
+                )
+            }
+        }
+
+        Row(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFB69DF8))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Row composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a horizontal sequence.",
+                    textAlign = TextAlign.Justify
+
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFF6EDFF))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Column composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a vertical sequence.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Pantalla 3")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onNext) {
+            Text("Volver al inicio")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewApp() {
+    ComposePracticeTheme {
+        AppContent()
+    }
+}
+
+
---
+++ src/main/java/com/example/composepractice/ThirdActivity.kt
@@ -0,0 +1,23 @@
+package com.example.composepractice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import android.content.Intent
+import android.widget.Button
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+
+        val btnRestart = findViewById<Button>(R.id.btnRestart)
+        btnRestart.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
